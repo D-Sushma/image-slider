2968b1339aca37c6c6afc84376469635d26b8455
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2022-12-20 16:53:26 +0530

    second commit...

--- src/components/Image_Slider.js
+++ src/components/Image_Slider.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import './Image-Slider.css';
+import slide1 from '../images/slide-1.jpg'
+import slide2 from '../images/slide-2.jpg'
+import slide3 from '../images/slide-3.jpg'
 import 'react-slideshow-image/dist/styles.css'
+// import AnimatedText from 'react-animated-text-content';
+// import TextTransition, { presets } from "react-text-transition";
+import MovingComponent from 'react-moving-text'
+
+
 //  ===================== 1.ONLY TO SLIDE IMAGE===================================== 
 import { Slide } from 'react-slideshow-image';
 // ------------------- 2. TO FAD IMAGE----------------------------------
 import { Fade } from 'react-slideshow-image';
 // ~~~~~~~~~~~~~~~~~~~~~ 3. TO ZOOM EFFECT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import { Zoom } from 'react-slideshow-image';
+
+
+// const TEXTS = [
+//     "Forest",
+//     "Building",
+//     "Tree",
+//     "Color"
+// ];
 
 // ===================== 1.ONLY TO SLIDE IMAGE=====================================
 // const slideImages = [
@@ -23,20 +39,38 @@ import { Zoom } from 'react-slideshow-image';
 // ];
 const zoomImages = [
     {
-        url: 'https://www.hdwallpaperspulse.com/wp-content/uploads/2014/05/27/523.jpeg',
-        caption: 'Slide 1'
+        // url: 'https://www.hdwallpaperspulse.com/wp-content/uploads/2014/05/27/523.jpeg',
+        // require:'../../public/images/',
+        url: slide1,
+        caption1: 'Slide 1',
+        caption2: 'Next-gen technology',
+        caption3: 'Data re-imagined'
     },
     {
-        url: 'https://www.themasterslawncare.com/sites/default/files/blog/good-day.jpg',
-        caption: 'Slide 2'
+        // url: 'https://www.themasterslawncare.com/sites/default/files/blog/good-day.jpg',
+        url: slide2,
+        caption1: 'Slide 2',
+        caption2: 'Delivering kindness in science',
+        caption3: 'Explore our animal platform'
     },
     {
-        url: 'https://thumbs.dreamstime.com/b/mother-daughter-sitting-table-colored-plasticine-modeling-looking-each-other-smiling-concept-s-day-233123829.jpg',
-        caption: 'Slide 3'
+        // url: 'https://thumbs.dreamstime.com/b/mother-daughter-sitting-table-colored-plasticine-modeling-looking-each-other-smiling-concept-s-day-233123829.jpg',
+        url: slide3,
+        caption1: 'Slide 3',
+        caption2: 'Precision in drug discovery'
     },
 ];
 
 export default function Image_Slider() {
+    // const [index, setIndex] = React.useState(0);
+
+    // React.useEffect(() => {
+    //     const intervalId = setInterval(() =>
+    //         setIndex(index => index + 1), 3000
+    //         // every 3 seconds
+    //     );
+    //     return () => clearTimeout(intervalId);
+    // }, []);
     return (
         <div className="slide-container">
             <h1><b>Image_Slider</b></h1>
@@ -68,8 +102,58 @@ export default function Image_Slider() {
                 {
                     zoomImages.map((zoomImage, index) => (
                         <div className="each-slide" key={index}>
-                            <img style={{ width: "100%" }} src={zoomImage.url} alt='' className='slide-image'/>
-                            <h2>{zoomImage.caption}</h2>
+                            <img style={{ width: "100%" }} src={zoomImage.url} alt='' className='slide-image' />
+                            <div className='caption'>
+                                <MovingComponent
+                                    type="blur"
+                                    duration="1000ms"
+                                    delay="0s"
+                                    direction="normal"
+                                    timing="ease"
+                                    iteration="5"
+                                    fillMode="none">
+                                    React-Moving-Text
+                                    <h1 className='caption1'>{zoomImage.caption1}</h1>
+                                    <h1 className='caption2'>{zoomImage.caption2}</h1>
+                                    <h1 className='caption3'>{zoomImage.caption3}</h1>
+                                </MovingComponent>               
+                                
+
+                                {/* <MovingText
+                                    type="animation_type"
+                                    duration="1000ms"
+                                    delay="0s"
+                                    direction="normal"
+                                    timing="ease"
+                                    iteration="infinite"
+                                    fillMode="none">
+                                    <p>Content...</p>
+                                </MovingText> */}
+
+                                {/* <TextTransition springConfig={presets.wobbly}>
+                                    {TEXTS[index % TEXTS.length]}
+                                </TextTransition> */}
+
+                                {/* <AnimatedText
+                                    type="words" // animate words or chars
+                                    animation={{
+                                        x: '200px',
+                                        y: '-20px',
+                                        scale: 1.1,
+                                        ease: 'ease-in-out',
+                                    }}
+                                    animationType="float"
+                                    interval={0.06}
+                                    duration={0.8}
+                                    tag="p"
+                                    className="animated-paragraph"
+                                    includeWhiteSpaces
+                                    threshold={0.1}
+                                    rootMargin="20%"
+                                >
+                                    Content to animate
+                                </AnimatedText>;  */}
+                            </div>
                         </div>
                     ))
                 }
